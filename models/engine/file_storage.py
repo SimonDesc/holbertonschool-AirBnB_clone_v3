f8182f73ac70641f5aef7454663ade6e8b3a8eb7
SimonDesc <pro.descombes@gmail.com>
2023-08-22 17:49:37 +0200

    feat: Update DBStorage and FileStorage, adding two new methods.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,25 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+            """A method to retrieve one object"""
+            dictionnary_object = self.__objects.items()
+            key_concat = cls.__name__ + "." + id
+
+            for key, value in dictionnary_object:
+                if key_concat == key:
+                    return value
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        dictionnary_object = self.__objects.items()
+        count = 0
+
+        if cls is None:
+            return len(dictionnary_object)
+        else:
+            for key, value in dictionnary_object:
+                if cls == type(value):
+                    count += 1
+        return count
