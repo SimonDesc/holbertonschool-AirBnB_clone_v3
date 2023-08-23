455df1af3a471cf8c58ab1ec1a6666fb1ebc63f4
SimonDesc <pro.descombes@gmail.com>
2023-08-23 14:04:03 +0200

    fix: add import to count all models

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-"""routes of index page"""
+"""Route of the index"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route("/status")
+@app_views.route('/status')
 def status():
-    """return the status of the API"""
+    """Returns the status of the API"""
     return jsonify(status="OK")
 
 
-@app_views.route("/stats")
+@app_views.route('/stats')
 def stats():
-    """show the number of class instances"""
-    object_stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
+    """Retrieves the number of each objects by type"""
+    objects_count = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
-    return jsonify(object_stats)
+    return jsonify(objects_count)
