86e98109d2baba2c553bb8dc7f99d3d9b1c4c8f4
SimonDesc <pro.descombes@gmail.com>
2023-08-23 13:36:17 +0200

    feat: creating first endpoint and return the status of the API

---
+++ api/v1/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""register blueprint and basics rule of flask app"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+from flask import jsonify
+
+"""create an instance of Flask"""
+app = Flask(__name__)
+
+
+"""register a blueprint"""
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(err):
+    """method called when the instance is at the end"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(e):
+    """message for 404"""
+    return jsonify(error="Not found"), 404
+
+
+"""define with the ENV the port and host"""
+if __name__ == "__main__":
+    host_hbnb = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port_hbnb = int(os.environ.get("HBNB_API_PORT", 5000))
+    app.run(host=host_hbnb, port=port_hbnb, threaded=True)
