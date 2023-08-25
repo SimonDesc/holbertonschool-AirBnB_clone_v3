fd00157dd5f86623c78c64b6d0d28718c63abd27
SimonDesc <pro.descombes@gmail.com>
2023-08-25 10:19:29 +0200

    feat: HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,10 +5,11 @@ from models import storage
 from api.v1.views import app_views
 import os
 from flask import jsonify
+from flask_cors import CORS
 
 """create an instance of Flask"""
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 """register a blueprint"""
 app.register_blueprint(app_views)
@@ -31,3 +32,4 @@ if __name__ == "__main__":
     host_hbnb = os.environ.get("HBNB_API_HOST", "0.0.0.0")
     port_hbnb = int(os.environ.get("HBNB_API_PORT", 5000))
     app.run(host=host_hbnb, port=port_hbnb, threaded=True)
+
