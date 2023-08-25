8397ece04a0e262c1aaff47773079d12a74f1f97
SimonDesc <pro.descombes@gmail.com>
2023-08-25 10:20:26 +0200

    fix: pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -32,4 +32,3 @@ if __name__ == "__main__":
     host_hbnb = os.environ.get("HBNB_API_HOST", "0.0.0.0")
     port_hbnb = int(os.environ.get("HBNB_API_PORT", 5000))
     app.run(host=host_hbnb, port=port_hbnb, threaded=True)
-
